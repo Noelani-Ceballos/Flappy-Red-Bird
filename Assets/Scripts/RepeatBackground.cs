d63216b53347c47b707715dc437f51d0e8c54511
Noelani-Ceballos <noelanic3@nycstudents.net>
2024-04-04 09:16:55 -0400

    The background is repeating.

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    public Vector3 startPos;
+
+    public float repeatWidth; 
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        repeatWidth = startPos.x - 3.50f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < repeatWidth)
+        {
+            transform.position = startPos;
+        }
     }
 }
