d521c87ba94167b841912ac8fd217ab24ac0085a
Noelani-Ceballos <noelanic3@nycstudents.net>
2024-04-16 09:08:10 -0400

    When the player hits a pipe or the ground the game stops

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -9,17 +9,20 @@ public class BirdController : MonoBehaviour
     Rigidbody2D bird;
     int score = 0;
     public Text scoreUI;
+    public bool isAlive;
 
     // Start is called before the first frame update
     void Start()
     {
         bird = GetComponent<Rigidbody2D>();
+        isAlive = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isAlive)
         {
             bird.AddForce(new Vector2(0,1) * 200);
         }
@@ -33,6 +36,11 @@ public class BirdController : MonoBehaviour
 
             scoreUI.text = score.ToString(); 
         }
+    } 
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        isAlive = false;
     }
 
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,18 +6,24 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject pipePrefab;
 
+    public BirdController birdScript; 
+
     float minY = 0.58f;
     float maxY = 6.52f;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnPipes", 2, 4);
+        birdScript = GameObject.Find("Red_Bird_1").GetComponent<BirdController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(birdScript.isAlive == false)
+        {
+            CancelInvoke();
+        }
     }
 
     void SpawnPipes()
